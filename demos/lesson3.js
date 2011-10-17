e019d99dfaedee54271d3f515bd4593f0b0a4d9c
Carlos Scheidegger <cscheid@research.att.com>
2011-10-17 16:45:33 -0400

    barebone pages

---
+++ demos/lesson3.js
@@ -0,0 +1,61 @@
+var gl;
+var square_drawable, triangle_drawable;
+var mvp; // model_view_projection
+
+//////////////////////////////////////////////////////////////////////////////
+
+function draw_it()
+{
+    var proj = Facet.perspective(45, 720/480, 0.1, 100.0);
+
+    mvp.set(mat4.product(proj, Facet.translation( 1.5, 0.0, -6.0)));
+    square_drawable.draw();
+
+    mvp.set(mat4.product(proj, Facet.translation(-1.5, 0.0, -6.0)));
+    triangle_drawable.draw();
+}
+
+$().ready(function () {
+    var canvas = document.getElementById("webgl");
+
+    gl = Facet.initGL(canvas,
+                      {
+                          clearDepth: 1.0,
+                          clearColor: [0,0,0,1],
+                          display: draw_it,
+                          attributes:
+                          {
+                              alpha: true,
+                              depth: true
+                          },
+                          debugging: true
+                      });
+
+    var square = Facet.model({
+        type: "triangles",
+        elements: [0, 1, 2, 0, 2, 3],
+        vertex: [[-1,-1, 1,-1, 1,1, -1,1], 2]
+    });
+    var triangle = Facet.model({
+        type: "triangles",
+        elements: [0, 1, 2],
+        vertex: [[0,1, -1,-1, 1,-1], 2],
+        color: [Shade.color('red'),
+                Shade.color('green'),
+                Shade.color('blue')]
+    });
+
+    mvp = Shade.uniform("mat4");
+
+    square_drawable = Facet.bake(square, {
+        position: mvp.mul(Shade.vec(square.vertex, 0, 1)),
+        color: Shade.color('#88f')
+    });
+
+    triangle_drawable = Facet.bake(triangle, {
+        position: mvp.mul(Shade.vec(triangle.vertex, 0, 1)),
+        color: triangle.color
+    });
+
+    gl.display();
+});
