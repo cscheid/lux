752c8305fcfe4f3b2e9b0267793472d0b2d87748
Carlos Scheidegger <cscheid@research.att.com>
2011-11-04 23:05:26 -0400

    ensures objects from _create have clean memoization caches

--- src/shade/memoize_on_field.js
+++ src/shade/memoize_on_field.js
@@ -4,12 +4,12 @@
 Shade.memoize_on_field = function(field_name, fun)
 {
     return function() {
-        if (typeOf(this[field_name]) === "undefined") {
-            this[field_name] = {};
+        if (typeOf(this._caches[field_name]) === "undefined") {
+            this._caches[field_name] = {};
         }
-        if (typeOf(this[field_name][arguments[0]]) === "undefined") {
-            this[field_name][arguments[0]] = fun.apply(this, arguments);
+        if (typeOf(this._caches[field_name][arguments[0]]) === "undefined") {
+            this._caches[field_name][arguments[0]] = fun.apply(this, arguments);
         }
-        return this[field_name][arguments[0]];
+        return this._caches[field_name][arguments[0]];
     };
 }
