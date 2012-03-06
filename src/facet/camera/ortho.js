f0d744a62779d98b45d4906c989a4c68cc838345
Carlos Scheidegger <cscheid@research.att.com>
2012-03-05 22:05:19 -0500

    Facet.Camera.ortho takes center-zoom specification

--- src/facet/camera/ortho.js
+++ src/facet/camera/ortho.js
@@ -11,12 +11,22 @@ Facet.Camera.ortho = function(opts)
     });
 
     var viewport_ratio = opts.aspect_ratio;
-    var left = opts.left;
-    var right = opts.right;
-    var bottom = opts.bottom;
-    var top = opts.top;
+    var left, right, bottom, top;
     var near = opts.near;
     var far = opts.far;
+
+    if (!_.isUndefined(opts.center) && !_.isUndefined(opts.zoom)) {
+        var viewport_width = Shade.div(1, opts.zoom);
+        left   = opts.center.at(0).sub(viewport_width);
+        right  = opts.center.at(0).add(viewport_width);
+        bottom = opts.center.at(1).sub(viewport_width);
+        top    = opts.center.at(1).add(viewport_width);
+    } else {
+        left = opts.left;
+        right = opts.right;
+        bottom = opts.bottom;
+        top = opts.top;
+    }
 
     function letterbox_projection() {
         var cy = Shade.add(top, bottom).div(2);
@@ -59,70 +69,4 @@ Facet.Camera.ortho = function(opts)
                 throw "expected vec, got " + t.repr();
         }
     };
-
-    // var proj_uniform = Shade.uniform("mat4");
-
-    // function update_projection()
-    // {
-    //         var view_ratio = (right - left) / (top - bottom);
-    //         var l, r, t, b;
-    //         var half_width, half_height;
-    //         if (view_ratio > viewport_ratio) {
-    //             // fat view rectangle, "letterbox" the projection
-    //             var cy = (top + bottom) / 2;
-    //             half_width = (right - left) / 2;
-    //             half_height = half_width / viewport_ratio;
-    //             l = left;
-    //             r = right;
-    //             t = cy + half_height;
-    //             b = cy - half_height;
-    //         } else {
-    //             // tall view rectangle, "pillarbox" the projection
-    //             var cx = (right + left) / 2;
-    //             half_height = (top - bottom) / 2;
-    //             half_width = half_height * viewport_ratio;
-    //             l = cx - half_width;
-    //             r = cx + half_width;
-    //             t = top;
-    //             b = bottom;
-    //         }
-    //     proj_uniform.set(mat4.ortho(l, r, b, t, near, far));
-    // }
-
-    // update_projection();
-
-    // return {
-    //     set_aspect_ratio: function(new_aspect_ratio) {
-    //         viewport_ratio = new_aspect_ratio;
-    //         update_projection();
-    //     },
-    //     set_bounds: function(opts) {
-    //         opts = _.defaults(opts, {
-    //             left: -1,
-    //             right: 1,
-    //             bottom: -1,
-    //             top: 1,
-    //             near: -1,
-    //             far: 1
-    //         });
-    //         left = opts.left;
-    //         right = opts.right;
-    //         bottom = opts.bottom;
-    //         top = opts.top;
-    //         near = opts.near;
-    //         far = opts.far;
-    //         update_projection();
-    //     },
-    //     project: function(model_vertex) {
-    //         var t = model_vertex.type;
-    //         if (t.equals(Shade.Types.vec2))
-    //             return proj_uniform.mul(Shade.vec(model_vertex, 0, 1));
-    //         else if (t.equals(Shade.Types.vec3))
-    //             return proj_uniform.mul(Shade.vec(model_vertex, 1));
-    //         else if (t.equals(Shade.Types.vec4))
-    //             return proj_uniform.mul(model_vertex);
-    //         else
-    //             throw "expected vec, got " + t.repr();
-    //     }
-    // };
 };
