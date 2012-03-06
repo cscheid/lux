17891a2a421271fc4c21503f1bdaadd2b4639ae2
Carlos Scheidegger <cscheid@research.att.com>
2012-03-05 23:11:10 -0500

    fbo texture resizing

--- src/facet/render_buffer.js
+++ src/facet/render_buffer.js
@@ -1,8 +1,7 @@
 Facet.render_buffer = function(opts)
 {
     var ctx = Facet._globals.ctx;
-    var rttFramebuffer = ctx.createFramebuffer();
-    ctx.bindFramebuffer(ctx.FRAMEBUFFER, rttFramebuffer);
+    var frame_buffer = ctx.createFramebuffer();
     opts = _.defaults(opts || {}, {
         width: 512,
         height: 512,
@@ -22,59 +21,67 @@ Facet.render_buffer = function(opts)
     // if (opts.width != opts.height)
     //     throw "renderbuffers must be square (blame GLSL ES!)";
 
-    rttFramebuffer.width  = opts.width;
-    rttFramebuffer.height = opts.height;
-
     var rttTexture = Facet.texture(opts);
 
-    var renderbuffer = ctx.createRenderbuffer();
-    ctx.bindRenderbuffer(ctx.RENDERBUFFER, renderbuffer);
-    ctx.renderbufferStorage(ctx.RENDERBUFFER, ctx.DEPTH_COMPONENT16, rttFramebuffer.width, rttFramebuffer.height);
+    frame_buffer.init = function(width, height) {
+        var ctx = Facet._globals.ctx;
+        this.width  = opts.width;
+        this.height = opts.height;
+        ctx.bindFramebuffer(ctx.FRAMEBUFFER, this);
+        var renderbuffer = ctx.createRenderbuffer();
+        ctx.bindRenderbuffer(ctx.RENDERBUFFER, renderbuffer);
+        ctx.renderbufferStorage(ctx.RENDERBUFFER, ctx.DEPTH_COMPONENT16, this.width, this.height);
 
-    ctx.framebufferTexture2D(ctx.FRAMEBUFFER, ctx.COLOR_ATTACHMENT0, ctx.TEXTURE_2D, rttTexture, 0);
-    ctx.framebufferRenderbuffer(ctx.FRAMEBUFFER, ctx.DEPTH_ATTACHMENT, ctx.RENDERBUFFER, renderbuffer);
-
-    var status = ctx.checkFramebufferStatus(ctx.FRAMEBUFFER);
-    switch (status) {
-        case ctx.FRAMEBUFFER_COMPLETE:
-            break;
-        case ctx.FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
-            throw "incomplete framebuffer: FRAMEBUFFER_INCOMPLETE_ATTACHMENT";
-        case ctx.FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
-            throw "incomplete framebuffer: FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT";
-        case ctx.FRAMEBUFFER_INCOMPLETE_DIMENSIONS:
-            throw "incomplete framebuffer: FRAMEBUFFER_INCOMPLETE_DIMENSIONS";
-        case ctx.FRAMEBUFFER_UNSUPPORTED:
-            throw "incomplete framebuffer: FRAMEBUFFER_UNSUPPORTED";
-        default:
-            throw "incomplete framebuffer: " + status;
-    }
-
-    ctx.bindTexture(ctx.TEXTURE_2D, null);
-    ctx.bindRenderbuffer(ctx.RENDERBUFFER, null);
-    ctx.bindFramebuffer(ctx.FRAMEBUFFER, null);
-
-    return {
-        _shade_type: 'render_buffer',
-        texture: rttTexture,
-        width: rttFramebuffer.width,
-        height: rttFramebuffer.height,
-        frame_buffer: rttFramebuffer,
-        with_bound_buffer: function (what) {
-            try {
-                ctx.bindFramebuffer(ctx.FRAMEBUFFER, rttFramebuffer);
-                ctx.viewport(0, 0, rttFramebuffer.width, rttFramebuffer.height);
-                return what();
-            } finally {
-                ctx.bindFramebuffer(ctx.FRAMEBUFFER, null);
+        ctx.framebufferTexture2D(ctx.FRAMEBUFFER, ctx.COLOR_ATTACHMENT0, ctx.TEXTURE_2D, rttTexture, 0);
+        ctx.framebufferRenderbuffer(ctx.FRAMEBUFFER, ctx.DEPTH_ATTACHMENT, ctx.RENDERBUFFER, renderbuffer);
+        var status = ctx.checkFramebufferStatus(ctx.FRAMEBUFFER);
+        try {
+            switch (status) {
+            case ctx.FRAMEBUFFER_COMPLETE:
+                break;
+            case ctx.FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
+                throw "incomplete framebuffer: FRAMEBUFFER_INCOMPLETE_ATTACHMENT";
+            case ctx.FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
+                throw "incomplete framebuffer: FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT";
+            case ctx.FRAMEBUFFER_INCOMPLETE_DIMENSIONS:
+                throw "incomplete framebuffer: FRAMEBUFFER_INCOMPLETE_DIMENSIONS";
+            case ctx.FRAMEBUFFER_UNSUPPORTED:
+                throw "incomplete framebuffer: FRAMEBUFFER_UNSUPPORTED";
+            default:
+                throw "incomplete framebuffer: " + status;
             }
-        },
-        make_screen_batch: function (with_texel_at_uv) {
-            var sq = Facet.Models.square();
-            return Facet.bake(sq, {
-                position: sq.vertex.mul(2).sub(1),
-                color: with_texel_at_uv(Shade.texture2D(rttTexture, sq.tex_coord), sq.tex_coord)
-            });
+        } finally {
+            ctx.bindTexture(ctx.TEXTURE_2D, null);
+            ctx.bindRenderbuffer(ctx.RENDERBUFFER, null);
+            ctx.bindFramebuffer(ctx.FRAMEBUFFER, null);
         }
     };
+
+    frame_buffer.init(opts.width, opts.height);
+    frame_buffer._shade_type = 'render_buffer';
+    frame_buffer.texture = rttTexture;
+    frame_buffer.resize = function(width, height) {
+        opts.width = width;
+        opts.height = height;
+        this.texture.init(opts);
+        this.init(width, height);
+    };
+    frame_buffer.with_bound_buffer = function(what) {
+        var ctx = Facet._globals.ctx;
+        try {
+            ctx.bindFramebuffer(ctx.FRAMEBUFFER, this);
+            ctx.viewport(0, 0, this.width, this.height);
+            return what();
+        } finally {
+            ctx.bindFramebuffer(ctx.FRAMEBUFFER, null);
+        }
+    };
+    frame_buffer.make_screen_batch = function(with_texel_at_uv) {
+        var sq = Facet.Models.square();
+        return Facet.bake(sq, {
+            position: sq.vertex.mul(2).sub(1),
+            color: with_texel_at_uv(Shade.texture2D(this.texture, sq.tex_coord), sq.tex_coord)
+        });
+    };
+    return frame_buffer;
 };
