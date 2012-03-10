4be2decc18f74aa9283197a987d2326dbd08bc9c
Carlos Scheidegger <cscheid@research.att.com>
2012-03-09 22:50:39 -0500

    perspective camera projection can be invoked with function call

--- src/facet/camera/perspective.js
+++ src/facet/camera/perspective.js
@@ -30,53 +30,54 @@ Facet.Camera.perspective = function(opts)
 
     update_projection();
 
-    return {
-        look_at: function(eye, to, up) {
-            current_view = mat4.lookAt(eye, to, up);
-            view_uniform.set(current_view);
-        },
-        set_aspect_ratio: function(a) {
-            aspect_ratio = a;
-            update_projection();
-            vp_uniform.set(Shade.mul(mat4.product(current_projection, current_view)));
-        },
-        set_near_distance: function(v) {
-            near_distance = v;
-            update_projection();
-            vp_uniform.set(Shade.mul(mat4.product(current_projection, current_view)));
-        },
-        set_far_distance: function(v) {
-            far_distance = v;
-            update_projection();
-            vp_uniform.set(Shade.mul(mat4.product(current_projection, current_view)));
-        },
-        set_field_of_view_y: function(v) {
-            field_of_view_y = v;
-            update_projection();
-            vp_uniform.set(Shade.mul(mat4.product(current_projection, current_view)));
-        },
-
-        project: function(model_vertex) {
-            var t = model_vertex.type;
-            if (t.equals(Shade.Types.vec2))
-                return vp_uniform.mul(Shade.vec(model_vertex, 0, 1));
-            else if (t.equals(Shade.Types.vec3))
-                return vp_uniform.mul(Shade.vec(model_vertex, 1));
-            else if (t.equals(Shade.Types.vec4))
-                return vp_uniform.mul(model_vertex);
-            else
-                throw "expected vec, got " + t.repr();
-        },
-        eye_vertex: function(model_vertex) {
-            var t = model_vertex.type;
-            if (t.equals(Shade.Types.vec2))
-                return view_uniform.mul(Shade.vec(model_vertex, 0, 1));
-            else if (t.equals(Shade.Types.vec3))
-                return view_uniform.mul(Shade.vec(model_vertex, 1));
-            else if (t.equals(Shade.Types.vec4))
-                return view_uniform.mul(model_vertex);
-            else
-                throw "expected vec, got " + t.repr();
-        }
+    var result = function(obj) {
+        return result.project(obj);
     };
+    result.look_at = function(eye, to, up) {
+        current_view = mat4.lookAt(eye, to, up);
+        view_uniform.set(current_view);
+    };
+    result.set_aspect_ratio = function(a) {
+        aspect_ratio = a;
+        update_projection();
+        vp_uniform.set(Shade.mul(mat4.product(current_projection, current_view)));
+    };
+    result.set_near_distance = function(v) {
+        near_distance = v;
+        update_projection();
+        vp_uniform.set(Shade.mul(mat4.product(current_projection, current_view)));
+    };
+    result.set_far_distance = function(v) {
+        far_distance = v;
+        update_projection();
+        vp_uniform.set(Shade.mul(mat4.product(current_projection, current_view)));
+    };
+    result.set_field_of_view_y = function(v) {
+        field_of_view_y = v;
+        update_projection();
+        vp_uniform.set(Shade.mul(mat4.product(current_projection, current_view)));
+    };
+    result.project = function(model_vertex) {
+        var t = model_vertex.type;
+        if (t.equals(Shade.Types.vec2))
+            return vp_uniform.mul(Shade.vec(model_vertex, 0, 1));
+        else if (t.equals(Shade.Types.vec3))
+            return vp_uniform.mul(Shade.vec(model_vertex, 1));
+        else if (t.equals(Shade.Types.vec4))
+            return vp_uniform.mul(model_vertex);
+        else
+            throw "expected vec, got " + t.repr();
+    };
+    result.eye_vertex = function(model_vertex) {
+        var t = model_vertex.type;
+        if (t.equals(Shade.Types.vec2))
+            return view_uniform.mul(Shade.vec(model_vertex, 0, 1));
+        else if (t.equals(Shade.Types.vec3))
+            return view_uniform.mul(Shade.vec(model_vertex, 1));
+        else if (t.equals(Shade.Types.vec4))
+            return view_uniform.mul(model_vertex);
+        else
+            throw "expected vec, got " + t.repr();
+    };
+    return result;
 };
