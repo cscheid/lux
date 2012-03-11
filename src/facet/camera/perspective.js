e5df2636af727161fc3b20ea7de4b8bcaa4f23b3
Carlos Scheidegger <cscheid@research.att.com>
2012-03-10 21:08:03 -0500

    perspective camera now out of Shade expressions

--- src/facet/camera/perspective.js
+++ src/facet/camera/perspective.js
@@ -2,7 +2,9 @@ Facet.Camera.perspective = function(opts)
 {
     opts = opts || {};
     opts = _.defaults(opts, {
-        look_at: [[0, 0, 0], [0, 0, -1], [0, 1, 0]],
+        look_at: [Shade.vec(0, 0, 0), 
+                  Shade.vec(0, 0, -1), 
+                  Shade.vec(0, 1, 0)],
         field_of_view_y: 45,
         aspect_ratio: 1,
         near_distance: 0.1,
@@ -14,70 +16,18 @@ Facet.Camera.perspective = function(opts)
     var near_distance = opts.near_distance;
     var far_distance = opts.far_distance;
 
-    var current_projection;
-    var current_view = mat4.lookAt(opts.look_at[0],
-                                   opts.look_at[1],
-                                   opts.look_at[2]);
-    var vp_parameter = Shade.parameter("mat4");
-    var view_parameter = Shade.parameter("mat4", current_view);
-
-    function update_projection()
-    {
-        current_projection = mat4.perspective(field_of_view_y, aspect_ratio,
-                                              near_distance, far_distance);
-        vp_parameter.set(Shade.mul(mat4.product(current_projection, current_view)));
-    }
-
-    update_projection();
-
+    var view = Shade.look_at(opts.look_at[0], opts.look_at[1], opts.look_at[2]);
+    var projection = Shade.perspective_matrix(field_of_view_y, aspect_ratio, near_distance, far_distance);
+    var vp_parameter = Shade.mul(projection, view);
     var result = function(obj) {
         return result.project(obj);
     };
-    result.look_at = function(eye, to, up) {
-        current_view = mat4.lookAt(eye, to, up);
-        view_parameter.set(current_view);
-    };
-    result.set_aspect_ratio = function(a) {
-        aspect_ratio = a;
-        update_projection();
-        vp_parameter.set(Shade.mul(mat4.product(current_projection, current_view)));
-    };
-    result.set_near_distance = function(v) {
-        near_distance = v;
-        update_projection();
-        vp_parameter.set(Shade.mul(mat4.product(current_projection, current_view)));
-    };
-    result.set_far_distance = function(v) {
-        far_distance = v;
-        update_projection();
-        vp_parameter.set(Shade.mul(mat4.product(current_projection, current_view)));
-    };
-    result.set_field_of_view_y = function(v) {
-        field_of_view_y = v;
-        update_projection();
-        vp_parameter.set(Shade.mul(mat4.product(current_projection, current_view)));
-    };
     result.project = function(model_vertex) {
-        var t = model_vertex.type;
-        if (t.equals(Shade.Types.vec2))
-            return vp_parameter.mul(Shade.vec(model_vertex, 0, 1));
-        else if (t.equals(Shade.Types.vec3))
-            return vp_parameter.mul(Shade.vec(model_vertex, 1));
-        else if (t.equals(Shade.Types.vec4))
-            return vp_parameter.mul(model_vertex);
-        else
-            throw "expected vec, got " + t.repr();
+        return vp_parameter.mul(model_vertex);
     };
     result.eye_vertex = function(model_vertex) {
         var t = model_vertex.type;
-        if (t.equals(Shade.Types.vec2))
-            return view_parameter.mul(Shade.vec(model_vertex, 0, 1));
-        else if (t.equals(Shade.Types.vec3))
-            return view_parameter.mul(Shade.vec(model_vertex, 1));
-        else if (t.equals(Shade.Types.vec4))
-            return view_parameter.mul(model_vertex);
-        else
-            throw "expected vec, got " + t.repr();
+        return view.mul(model_vertex);
     };
     return result;
 };
