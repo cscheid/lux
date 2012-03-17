c03d51c81a2510d944adab3f4ffa70cc539137af
Carlos Scheidegger <cscheid@research.att.com>
2012-03-17 16:02:30 -0400

    texture_table now uses texture_array, texture_array is now faster. To do this the right way we'll need better API support from the TypedArray spec.

--- src/facet/data/texture_array.js
+++ src/facet/data/texture_array.js
@@ -1,3 +1,34 @@
+/*
+   texture array takes an object with fields:
+
+     n_cols (integer): number of columns in the 2D array of data
+     n_rows (integer): number of rows in the 2D array of data
+     elements (array, Float32Array): list of elements in the array
+
+s parameters a list of floating point elements
+   (or a Float32Array), the number of columns and rows in the implied 2D array of data
+
+   and returns an object with four fields:
+
+   n_cols (integer): number of columns in the data
+
+   n_rows (integer): number of rows in the data
+
+   at (function(Shade(int), Shade(int)) -> Shade(float)): returns the
+   value stored at given row and column
+
+   index (function(Shade(int), Shade(int)) -> Shade(vec3)): returns
+   the index of the value stored at given row and column. This is a
+   three dimensional vector.  The first two coordinates store the
+   texture coordinate, and the fourth coordinate stores the
+   channel. This is necessary to take advantage of RGBA float
+   textures, which have the widest support on WebGL-capable hardware.
+
+   For example, luminance float textures appear to clamp to [0,1], at
+   least on Chrome 15 on Linux.
+
+ */
+
 Facet.Data.texture_array = function(opts)
 {
     var ctx = Facet._globals.ctx;
@@ -5,65 +36,59 @@ Facet.Data.texture_array = function(opts)
     var n_cols = opts.n_cols;
     var n_rows = opts.n_rows;
 
-    var table = {};
-    table.number_columns = _.range(0, n_cols);
-    table.columns = _.range(0, n_cols);
-    table.is_numeric_row_complete = function() { return true; };
-    table.data = [];
-    for (var i=0; i<n_rows; ++i) {
-        var lst = [];
-        for (var j=0; j<n_cols; ++j) {
-            lst.push(opts.elements[i*n_cols + j]);
-        }
-        table.data.push(lst);
+    var texture_width = 1;
+    while (4 * texture_width * texture_width < elements.length) {
+        texture_width = texture_width * 2;
     }
-    return Facet.Data.texture_table(table);
+    var texture_height = Math.ceil(elements.length / (4 * texture_width));
 
-    // var texture_width = 1;
-    // while (4 * texture_width * texture_width < opts.elements.length) {
-    //     texture_width = texture_width * 2;
-    // }
-    // var texture_height = Math.ceil(elements.length / (4 * texture_width));
+    var new_elements;
+    if (texture_width * texture_height === elements.length) {
+        // no chance this will ever happen in practice, but hey, 
+        // a man can dream
+        if (facet_typeOf(elements) === "array") {
+            new_elements = new Float32Array(elements);
+        } else
+            new_elements = elements;
+    } else {
+        new_elements = new Float32Array(texture_width * texture_height * 4);
+        for (var i=0; i<elements.length; ++i)
+            new_elements[i] = elements[i];
+    }
 
-    // console.log("Texture size: ", texture_width, texture_height);
+    var texture = Facet.texture({
+        width: texture_width,
+        height: texture_height,
+        buffer: new_elements,
+        type: ctx.FLOAT,
+        format: ctx.RGBA,
+        min_filter: ctx.NEAREST,
+        mag_filter: ctx.NEAREST
+    });
 
-    // // FIXME poking at the input is bad form.
-    // if (elements.length < 4 * texture_height * texture_width)
-    //     elements[4 * texture_height * texture_width - 1] = 0;
-    
-    // var texture = Facet.texture({
-    //     width: texture_width,
-    //     height: texture_height,
-    //     buffer: new Float32Array(elements),
-    //     type: ctx.FLOAT,
-    //     format: ctx.RGBA,
-    //     min_filter: ctx.NEAREST,
-    //     mag_filter: ctx.NEAREST
-    // });
+    var index = Shade(function(row, col) {
+        var linear_index    = row.mul(n_cols).add(col);
+        var in_texel_offset = linear_index.mod(4);
+        var texel_index     = linear_index.div(4).floor();
+        var x               = texel_index.mod(texture_width);
+        var y               = texel_index.div(texture_width).floor();
+        var result          = Shade.vec(x, y, in_texel_offset);
+        return result;
+    });
+    var at = Shade(function(row, col) {
+        // returns Shade expression with value at row, col
+        var ix = index(row, col);
+        var uv = ix.swizzle("xy")
+            .add(Shade.vec(0.5, 0.5))
+            .div(Shade.vec(texture_width, texture_height))
+            ;
+        return Shade.texture2D(texture, uv).at(ix.z());
+    });
 
-    // var index = Shade(function(row, col) {
-    //     var linear_index    = row.mul(n_cols).add(col);
-    //     var in_texel_offset = linear_index.mod(4);
-    //     var texel_index     = linear_index.div(4).floor();
-    //     var x               = texel_index.mod(texture_width);
-    //     var y               = texel_index.div(texture_width).floor();
-    //     var result          = Shade.vec(x, y, in_texel_offset);
-    //     return result;
-    // });
-    // var at = Shade(function(row, col) {
-    //     // returns Shade expression with value at row, col
-    //     var ix = index(row, col);
-    //     var uv = ix.swizzle("xy")
-    //         .add(Shade.vec(0.5, 0.5))
-    //         .div(Shade.vec(texture_width, texture_height))
-    //         ;
-    //     return Shade.texture2D(texture, uv).at(ix.z());
-    // });
-
-    // return {
-    //     n_rows: n_rows,
-    //     n_cols: n_cols,
-    //     at: at,
-    //     index: index
-    // };
+    return {
+        n_rows: n_rows,
+        n_cols: n_cols,
+        at: at,
+        index: index
+    };
 };
--- src/facet/data/texture_table.js
+++ src/facet/data/texture_table.js
@@ -1,6 +1,3 @@
-// NB: Luminance float textures appear to clamp to [0,1] on Chrome 15
-// on Linux...
-
 Facet.Data.texture_table = function(table)
 {
     var ctx = Facet._globals.ctx;
@@ -24,50 +21,9 @@ Facet.Data.texture_table = function(table)
     var table_nrows = elements.length / table.number_columns.length;
     var texture_width = 1;
 
-    while (4 * texture_width * texture_width < elements.length) {
-        texture_width = texture_width * 2;
-    }
-    var texture_height = Math.ceil(elements.length / (4 * texture_width));
-
-    // Tested on Chrome: the Float32Array constructor interprets "undefined" as 0
-    // so no push to pad array is necessary; we simply set the last
-    // padded value of the array to 0.
-    if (elements.length < 4 * texture_height * texture_width)
-        elements[4 * texture_height * texture_width - 1] = 0;
-
-    var texture = Facet.texture({
-        width: texture_width,
-        height: texture_height,
-        buffer: new Float32Array(elements),
-        type: ctx.FLOAT,
-        format: ctx.RGBA,
-        min_filter: ctx.NEAREST,
-        mag_filter: ctx.NEAREST
-    });
-
-    var index = Shade(function(row, col) {
-        var linear_index    = row.mul(table_ncols).add(col);
-        var in_texel_offset = linear_index.mod(4);
-        var texel_index     = linear_index.div(4).floor();
-        var x               = texel_index.mod(texture_width);
-        var y               = texel_index.div(texture_width).floor();
-        var result          = Shade.vec(x, y, in_texel_offset);
-        return result;
-    });
-    var at = Shade(function(row, col) {
-        // returns Shade expression with value at row, col
-        var ix = index(row, col);
-        var uv = ix.swizzle("xy")
-            .add(Shade.vec(0.5, 0.5))
-            .div(Shade.vec(texture_width, texture_height))
-            ;
-        return Shade.texture2D(texture, uv).at(ix.z());
-    });
-
-    return {
+    return Facet.Data.texture_array({
         n_rows: table_nrows,
         n_cols: table_ncols,
-        at: at,
-        index: index
-    };
+        elements: elements
+    });
 };
