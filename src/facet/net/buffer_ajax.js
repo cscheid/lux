f0a5adcc7b2ed5899d828642cc269f7bcfe42938
Carlos Scheidegger <cscheid@research.att.com>
2012-03-17 16:12:42 -0400

    move many-buffer request functionality into Facet.Net.buffer_ajax

--- src/facet/net/buffer_ajax.js
+++ src/facet/net/buffer_ajax.js
@@ -1,6 +1,37 @@
+/*
+ * Facet.Net.buffer_ajax issues binary AJAX requests, which can be
+ * used to load data into Facet more efficiently than through the
+ * regular text or JSON AJAX interfaces.
+ * 
+ * It takes as parameters
+ * 
+ *  url (string or list of strings): urls to fetch
+ * 
+ *  handler (function(buffer or dictionary of (url: buffer))): a callback
+ *  which gets invoked when all requests finish. If a single URL was passed,
+ *  the callback is called with the single buffer returned. If a list of URLs
+ *  were passed, then an object is returned, mapping the URLs as passed to
+ *  the buffers.
+ *  
+ * FIXME Facet.Net.buffer_ajax has no error handling.
+ */
+
 // based on http://calumnymmo.wordpress.com/2010/12/22/so-i-decided-to-wait/
 Facet.Net.buffer_ajax = function(url, handler)
 {
+    if (facet_typeOf(url) === "array") {
+        var obj = {};
+        var done = _.after(url.length, handler);
+        function piecemeal_handler(buffer, internal_url) {
+            obj[internal_url] = buffer;
+            done(obj);
+        }
+        _.each(url, function(internal_url) {
+            Facet.Net.buffer_ajax(internal_url, piecemeal_handler);
+        });
+        return;
+    }
+
     var xhr = new window.XMLHttpRequest();
     var ready = false;
     xhr.onreadystatechange = function() {
