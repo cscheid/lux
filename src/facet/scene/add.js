c38511765a0866bc60bb853eb52f3c7b15ac9106
Carlos Scheidegger <cscheid@research.att.com>
2012-03-18 19:35:53 -0400

    scene object needs no batch_id because of strict equality comparison on remove

--- src/facet/scene/add.js
+++ src/facet/scene/add.js
@@ -2,10 +2,6 @@ Facet.Scene.add = function(obj)
 {
     var scene = Facet._globals.ctx._facet_globals.scene;
 
-    if (_.isUndefined(obj.batch_id)) {
-        throw "Expected a batch, got an object without a batch_id";
-    }
-    var batch_id = obj.batch_id;
     scene.push(obj);
     Facet.Scene.invalidate();
 };
