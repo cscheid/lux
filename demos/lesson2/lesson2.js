ea4b6b0c24c637504d242346a24680c33bee7a45
Carlos Scheidegger <cscheid@research.att.com>
2012-03-17 21:10:06 -0400

    lesson 2 uses scene api

--- demos/lesson2/lesson2.js
+++ demos/lesson2/lesson2.js
@@ -3,14 +3,7 @@ var square, triangle;
 
 //////////////////////////////////////////////////////////////////////////////
 
-function draw_it()
-{
-    square.draw();
-    triangle.draw();
-}
-
 $().ready(function () {
-    var canvas = document.getElementById("webgl");
     var camera = Shade.Camera.perspective({
         look_at: [Shade.vec(0, 0, 6), Shade.vec(0, 0, -1), Shade.vec(0, 1, 0)],
         field_of_view_y: 45,
@@ -19,14 +12,8 @@ $().ready(function () {
         far_distance: 100
     });
 
-    gl = Facet.init(canvas, {
-        clearDepth: 1.0,
-        clearColor: [0,0,0,0.2],
-        display: draw_it,
-        attributes: {
-            alpha: true,
-            depth: true
-        }
+    gl = Facet.init(document.getElementById("webgl"), {
+        clearColor: [0, 0, 0, 0.2]
     });
 
     var square_model = Facet.model({
@@ -39,17 +26,14 @@ $().ready(function () {
         vertex: [[0,1, -1,-1, 1,-1], 2]
     });
 
-    square = Facet.bake(square_model, {
+    Facet.Scene.add(Facet.bake(square_model, {
         position: camera(
             Shade.translation(Shade.vec(1.5, 0, 0)).mul(square_model.vertex)),
         color: Shade.color('white')
-    });
-
-    triangle = Facet.bake(triangle_model, {
+    }));
+    Facet.Scene.add(Facet.bake(triangle_model, {
         position: camera(
             Shade.translation(Shade.vec(-1.5, 0, 0)).mul(triangle_model.vertex)),
         color: Shade.color('white')
-    });
-
-    gl.display();
+    }));
 });
