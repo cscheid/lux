0979e36e76d2f628c5e0fd33e2adf5a42c442cc2
Carlos Scheidegger <cscheid@research.att.com>
2012-04-20 15:32:28 -0400

    better teapot demo

--- demos/teapot/teapot.js
+++ demos/teapot/teapot.js
@@ -14,7 +14,7 @@ function draw_it()
 $().ready(function () {
     var canvas = document.getElementById("webgl");
     var camera = Shade.Camera.perspective({
-        look_at: [Shade.vec(0, 0, 6), Shade.vec(0, 0, -1), Shade.vec(0, 1, 0)],
+        look_at: [Shade.vec(0, 2, 20), Shade.vec(0, 0, -1), Shade.vec(0, 1, 0)],
         field_of_view_y: 45,
         aspect_ratio: 720/480,
         near_distance: 0.1,
@@ -32,9 +32,19 @@ $().ready(function () {
     });
     var teapot_model = Models.teapot();
 
+    var model_matrix = (Shade.rotation(0.3, Shade.vec(1,0,0)))(Shade.rotation(angle, Shade.vec(0,1,0)));
+    var model_vertex = model_matrix.mul(teapot_model.vertex);
+
     teapot = Facet.bake(teapot_model, {
-        position: camera(Shade.rotation(angle, Shade.vec(1,1,1)).mul(teapot_model.vertex)),
-        color: Shade.vec(Shade.abs(teapot_model.vertex), 1)
+        position: camera(model_vertex),
+        color: Shade.gl_light({
+            light_position: Shade.vec(5,5,10),
+            material_color: Shade.color("white"),
+            light_ambient: Shade.vec(0.1, 0.1, 0.1, 1.0),
+            light_diffuse: Shade.color("white"),
+            vertex: model_vertex,
+            normal: model_matrix(teapot_model.normal)
+        })
     });
 
     var start = new Date().getTime();
