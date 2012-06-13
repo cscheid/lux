00995889871b9bf0e45ff6a6c5df1040f1be5b8a
Carlos Scheidegger <cscheid@research.att.com>
2012-06-13 14:54:39 -0400

    more refactoring

--- demos/beauty_of_roots/beauty_of_roots.js
+++ demos/beauty_of_roots/beauty_of_roots.js
@@ -20,12 +20,13 @@ function center_zoom_interactor(opts)
     var width = opts.width;
     var center = Shade.parameter("vec2", opts.center);
     var zoom = Shade.parameter("float", opts.zoom);
-    
     var prev_mouse_pos;
+
     function mouse_down(event) {
         prev_mouse_pos = [event.offsetX, event.offsetY];
         opts.mouse_down(event);
     }
+
     function mouse_move(event) {
         if ((event.which & 1) && !event.shiftKey) {
             var deltaX =  (event.offsetX - prev_mouse_pos[0]) / (height * zoom.get() / 2);
@@ -39,6 +40,7 @@ function center_zoom_interactor(opts)
         opts.mouse_move(event);
         Facet.Scene.invalidate();
     }
+
     function mouse_wheel(event, delta, deltaX, deltaY) {
         zoom.set(zoom.get() * (1.0 - deltaY / 15));
         opts.mouse_wheel(event, delta, deltaX, deltaY);
@@ -70,20 +72,6 @@ function center_zoom_interactor(opts)
     };
 }
 
-function draw_it()
-{
-    rb.with_bound_buffer(function() {
-        gl.clear(gl.COLOR_BUFFER_BIT);
-        if (points_batch)
-            points_batch.draw();
-    });
-    rb_batch.draw();
-}
-
-function update_camera() {
-    gl.display();
-};
-
 function parameter_slider(element, parameter, opts)
 {
     opts = _.defaults(opts, {
@@ -94,8 +82,7 @@ function parameter_slider(element, parameter, opts)
         change: function() {}
     });
 
-    var slider_min = 0,
-        slider_max = 1000;
+    var slider_min = 0, slider_max = 1000;
 
     function to_slider(v) {
         return (v-opts.min) / (opts.max - opts.min) * 
@@ -126,6 +113,49 @@ function parameter_slider(element, parameter, opts)
     });
 }
 
+function make_points_batch(x, y, width, height)
+{
+    var points_model = Facet.model({
+        x: x,
+        y: y,
+        type: "points"
+    });
+    var pt = Shade.vec(points_model.x, points_model.y);
+
+    rb = Facet.render_buffer({ width: width, height: height, type: gl.FLOAT });
+    rb_batch = rb.make_screen_batch(function(texel_at_uv) {
+        return Shade.vec(1,1,1,2)
+            .sub(Shade.Utils.lerp([Shade.color("white"),
+                                   Shade.color("#d29152"),
+                                   Shade.color("sienna"),
+                                   Shade.color("black")])(texel_at_uv.at(0).add(1).log()));
+    });
+
+    var batch = Facet.bake(points_model, {
+        position: interactor.camera(pt),
+        mode: Facet.DrawingMode.additive,
+        color: Shade.pointCoord().sub(Shade.vec(0.5, 0.5))
+            .norm().pow(2).neg()
+            .mul(20)
+            .exp()
+            .mul(pointweight)
+            .mul(interactor.zoom.pow(0.33))
+            .mul(Shade.color("white")),
+        point_size: interactor.zoom.pow(0.5).mul(pointsize)
+    });
+
+    return {
+        draw: function() {
+            rb.with_bound_buffer(function() {
+                gl.clear(gl.COLOR_BUFFER_BIT);
+                if (batch)
+                    batch.draw();
+            });
+            rb_batch.draw();
+        }
+    };
+}
+
 function init_gui()
 {
     parameter_slider("#pointsize",   pointsize,   { min: 0, max: 10 });
@@ -136,7 +166,7 @@ function init_gui()
             y = Number($("#imagvalue").val());
         if (!isNaN(x) && !isNaN(y)) {
             interactor.center.set(vec.make([x, y]));
-            update_camera();
+            Facet.Scene.invalidate();
         }
     });
     $(window).resize(function(eventObject) {
@@ -147,8 +177,14 @@ function init_gui()
         interactor.resize(w, h);
         gl.resize(w, h);
         rb.resize(w, h);
-        gl.display();
+        Facet.Scene.invalidate();
     });
+    $("#greeting").click(function() {
+        $("#greeting").fadeOut(500);
+    });
+    window.setTimeout(function() {
+        $("#greeting").fadeOut(500);
+    }, 15000);
 }
 
 $().ready(function() {
@@ -156,22 +192,10 @@ $().ready(function() {
     pointweight = Shade.parameter("float", 0.5);
     init_gui();
 
-    $("#greeting").click(function() {
-        $("#greeting").fadeOut(500);
-    });
-
-    window.setTimeout(function() {
-        $("#greeting").fadeOut(500);
-    }, 15000);
-
-    var prev_mouse_pos;
     var canvas = document.getElementById("webgl");
     var width = window.innerWidth, height = window.innerHeight;
     canvas.width = width;
     canvas.height = height;
-
-    center = Shade.parameter("vec2", vec.make([0, 0]));
-    zoom = Shade.parameter("float", 2/3);
 
     interactor = center_zoom_interactor({
         width: width,
@@ -183,7 +207,6 @@ $().ready(function() {
     gl = Facet.init(canvas, {
         clearDepth: 1.0,
         clearColor: [0,0,0,1],
-        display: draw_it,
         attributes: {
             alpha: true,
             depth: true
@@ -191,43 +214,16 @@ $().ready(function() {
         mousedown: interactor.mouse_down,
         mousemove: interactor.mouse_move
     });
-
     $(canvas).bind('mousewheel', interactor.mouse_wheel);
-
-    rb = Facet.render_buffer({ width: width, height: height, type: gl.FLOAT });
-    rb_batch = rb.make_screen_batch(function(texel_at_uv) {
-        return Shade.vec(1,1,1,2)
-            .sub(Shade.Utils.lerp([Shade.color("white"),
-                                   Shade.color("#d29152"),
-                                   Shade.color("sienna"),
-                                   Shade.color("black")])(texel_at_uv.at(0).add(1).log()));
-    });
 
     Facet.Net.binary(["data/roots_real.raw", "data/roots_imag.raw"], function (obj) {
         var x = Facet.attribute_buffer({ vertex_array: new Float32Array(obj["data/roots_real.raw"]), item_size: 1});
         var y = Facet.attribute_buffer({ vertex_array: new Float32Array(obj["data/roots_imag.raw"]), item_size: 1});
-        var points_model = Facet.model({
-            x: x,
-            y: y,
-            type: "points"
-        });
-        var pt = Shade.vec(points_model.x, points_model.y);
-        points_batch = Facet.bake(points_model, {
-            position: interactor.camera(pt),
-            mode: Facet.DrawingMode.additive,
-            
-            //color: Shade.round_dot(Shade.vec(0.1,0,0,1)),
-            color: Shade.pointCoord().sub(Shade.vec(0.5, 0.5))
-                .norm().pow(2).neg()
-                .mul(20)
-                .exp()
-                .mul(pointweight)
-                .mul(interactor.zoom.pow(0.33))
-                .mul(Shade.color("white")),
-            gl_PointSize: interactor.zoom.pow(0.5).mul(pointsize)
-        });
+        points_batch = make_points_batch(x, y, width, height);
+
         $("#loading").fadeOut(500);
-        gl.display();
+        Facet.Scene.add(points_batch);
+        Facet.Scene.invalidate();
     });
-    gl.display();
+    Facet.Scene.invalidate();
 });
