249bc9a07607ef712a45571a5c813dfbffec889c
Carlos Scheidegger <cscheid@research.att.com>
2012-07-30 11:17:39 -0400

    update

--- facet.js
+++ facet.js
@@ -8920,6 +8920,7 @@ Shade.Utils.choose = function(lst) {
 };
 Shade.Utils.linear = function(f1, f2, t1, t2)
 {
+    console.log("Shade.Utils.linear is deprecated; use Shade.Scale.linear instead");
     var df = Shade.sub(f2, f1), dt = Shade.sub(t2, t1);
     return function(x) {
         return Shade.make(x).sub(f1).mul(dt.div(df)).add(t1);
