447b8fe772e99b2e9690e3682a92e2cceeeef2f5
Carlos Scheidegger <cscheid@research.att.com>
2012-07-30 11:17:30 -0400

    deprecate shade.utils.linear

--- src/shade/utils/linear.js
+++ src/shade/utils/linear.js
@@ -1,5 +1,6 @@
 Shade.Utils.linear = function(f1, f2, t1, t2)
 {
+    console.log("Shade.Utils.linear is deprecated; use Shade.Scale.linear instead");
     var df = Shade.sub(f2, f1), dt = Shade.sub(t2, t1);
     return function(x) {
         return Shade.make(x).sub(f1).mul(dt.div(df)).add(t1);
