4cb015d8dcccb03e5a1b22a435bc12b73b72b967
Carlos Scheidegger <cscheid@research.att.com>
2012-08-15 18:36:55 -0400

    more mercator transformations

--- src/shade/scale/geo/latlong_to_spherical.js
+++ src/shade/scale/geo/latlong_to_spherical.js
@@ -1,10 +1,7 @@
-// FIXME can't be Shade(function()...) because Shade() hasn't been defined yet.
-Shade.Scale.Geo.latlong_to_spherical = function(lat, lon)
+Shade.Scale.Geo.latlong_to_spherical = Shade(function(lat, lon)
 {
-    lat = Shade(lat);
-    lon = Shade(lon);
     var stretch = lat.cos();
     return Shade.vec(lon.sin().mul(stretch),
                      lat.sin(),
                      lon.cos().mul(stretch), 1);
-};
+});
