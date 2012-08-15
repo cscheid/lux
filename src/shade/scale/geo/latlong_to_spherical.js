9f2e3dd93a67cb669a57cf43bbe224a20dc4a9cb
Carlos Scheidegger <cscheid@research.att.com>
2012-08-15 18:18:40 -0400

    moved Facet.Scale.Geo to Shade.Scale

--- src/shade/scale/geo/latlong_to_spherical.js
+++ src/shade/scale/geo/latlong_to_spherical.js
@@ -1,7 +1,5 @@
 // FIXME can't be Shade(function()...) because Shade() hasn't been defined yet.
-//
-// FIXME this means that Facet.Scale should, unsurprisingly, be Shade.Scale.
-Facet.Scale.Geo.latlong_to_spherical = function(lat, lon)
+Shade.Scale.Geo.latlong_to_spherical = function(lat, lon)
 {
     lat = Shade(lat);
     lon = Shade(lon);
