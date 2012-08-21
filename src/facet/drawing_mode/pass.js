6ba3086fbc5c1b062e64d9f15dd1c988a4f00f13
Carlos Scheidegger <cscheid@research.att.com>
2012-08-21 15:12:39 -0400

    force reset blending modes

--- src/facet/drawing_mode/pass.js
+++ src/facet/drawing_mode/pass.js
@@ -4,17 +4,20 @@ Facet.DrawingMode.pass = {
         var ctx = Facet._globals.ctx;
         ctx.disable(ctx.DEPTH_TEST);
         ctx.depthMask(false);
+        ctx.disable(ctx.BLEND);
     },
     set_pick_caps: function()
     { 
         var ctx = Facet._globals.ctx;
         ctx.disable(ctx.DEPTH_TEST);
         ctx.depthMask(false);
+        ctx.disable(ctx.BLEND);
     },
     set_unproject_caps: function()
     {
         var ctx = Facet._globals.ctx;
         ctx.disable(ctx.DEPTH_TEST);
         ctx.depthMask(false);
+        ctx.disable(ctx.BLEND);
     }
 };
