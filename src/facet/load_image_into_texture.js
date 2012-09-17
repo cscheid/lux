8046708451f153f8b94d725d99f486e3b445be9b
Carlos Scheidegger <cscheid@research.att.com>
2012-09-17 18:57:58 -0400

    canvas support, docs

--- src/facet/load_image_into_texture.js
+++ src/facet/load_image_into_texture.js
@@ -1,3 +1,60 @@
+/*
+ * Facet.load_image_into_texture:
+ * 
+ *   Replaces a rectangle of a Facet texture with a given image.
+ * 
+ *   This is useful to store a large set of rectangular images into a single texture, for example.
+ * 
+ *   Example usage:
+ * 
+ *   * Load an image from a URL:
+ * 
+ *     Facet.load_image_into_texture({ 
+ *       texture: texture,
+ *       src: "http://www.example.com/image.png"
+ *     })
+ * 
+ *   * Invoke a callback when image is successfully loaded:
+ * 
+ *     Facet.load_image_into_texture({ 
+ *       texture: texture,
+ *       src: "http://www.example.com/image.png",
+ *       onload: function() { alert("image has now loaded!") }
+ *     })
+ * 
+ *   * Specify an offset:
+ * 
+ *     Facet.load_image_into_texture({ 
+ *       texture: texture,
+ *       src: "http://www.example.com/image.png",
+ *       x_offset: 64,
+ *       y_offset: 32
+ *     })
+ * 
+ *   * Load an image from an existing element in the DOM:
+ * 
+ *     Facet.load_image_into_texture({
+ *       texture: texture,
+ *       img: document.getElementById("image-element")
+ *     });
+ *
+ *     Facet.load_image_into_texture({
+ *       texture: texture,
+ *       canvas: document.getElementById("canvas-element")
+ *     });
+ * 
+ *   * Load an image from a TypedArray buffer (currently only supports 8-bit RGBA):
+ * 
+ *     Facet.load_image_into_texture({
+ *       texture: texture,
+ *       width: 128,
+ *       height: 128,
+ *       buffer: new Uint8Array(128 * 128 * 4)
+ *     });
+ */
+
+// FIXME: move all of this code so that it's a method in Facet.texture.
+
 Facet.load_image_into_texture = function(opts)
 {
     opts = _.defaults(opts, {
@@ -46,6 +103,8 @@ Facet.load_image_into_texture = function(opts)
         if (opts.crossOrigin)
             image.crossOrigin = opts.crossOrigin;
         image.src = opts.src;
+    } else if (opts.canvas) {
+        image_handler(opts.canvas);
     } else if (opts.img) {
         if (opts.img.isComplete) {
             image_handler(opts.img);
