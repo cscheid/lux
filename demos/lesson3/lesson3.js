778f5640d72f9e73572fd804e9265e40d8c2a0bd
Carlos Scheidegger <cscheid@research.att.com>
2013-01-25 21:15:21 -0500

    merge

--- demos/lesson3/lesson3.js
+++ demos/lesson3/lesson3.js
@@ -2,27 +2,29 @@ $().ready(function () {
     var gl = Facet.init(document.getElementById("webgl"), {
         clearColor: [0,0,0,0.2]
     });
+    var camera = Shade.Camera.perspective();
 
     var square = Facet.model({
         type: "triangles",
         elements: [0, 1, 2, 0, 2, 3],
         vertex: [[-1,-1, 1,-1, 1,1, -1,1], 2]
     });
-    var triangle = Facet.model({
-        type: "triangles",
-        elements: [0, 1, 2],
-        vertex: [[0,1, -1,-1, 1,-1], 2],
-        color: [Shade.color('red'),
-                Shade.color('green'),
-                Shade.color('blue')]
-    });
-
-    var camera = Shade.Camera.perspective();
-
     Facet.Scene.add(Facet.bake(square, {
         position: camera(Shade.translation( 1.5, 0, -6)(square.vertex)),
         color: Shade.color('#88f')
     }));
+
+    var triangle = Facet.model({
+        type: "triangles",
+        elements: [0, 1, 2],
+        vertex: [[0,1, -1,-1, 1,-1], 2],
+        color: [[1,0,0,1, 0,0.5,0,1, 0,0,1,1], 4]
+        // color: [Shade.color('red'),
+        //         Shade.color('green'),
+        //         Shade.color('blue')]
+    });
+
+
     Facet.Scene.add(Facet.bake(triangle, {
         position: camera(Shade.translation(-1.5, 0, -6)(triangle.vertex)),
         color: triangle.color
