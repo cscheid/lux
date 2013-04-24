d397c798655b15c79db175cae4836c081b82b209
Carlos Scheidegger <cscheid@research.att.com>
2013-04-24 12:05:23 -0400

    minor doc changes

--- src/typeinfo.js
+++ src/typeinfo.js
@@ -23,7 +23,7 @@ function lux_constant_type(obj)
 // it is convenient in many places to accept as a parameter a scalar,
 // a vector or a matrix. This function tries to
 // tell them apart. Functions such as vec.make and mat.make populate
-// the _type slot. This is ugly, but extremely convenient.
+// the .buffer._type slot. This is ugly, but extremely convenient.
 {
     var t = typeof obj;
     if (t === "boolean")         return "boolean";
