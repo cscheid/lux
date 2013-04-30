4170886f1a7c5b95ed497f11b329c84616b8bcdc
Carlos Scheidegger <cscheid@research.att.com>
2013-04-30 15:56:16 -0400

    api tweak for Shade.Light.diffuse

--- src/shade/light/diffuse.js
+++ src/shade/light/diffuse.js
@@ -8,17 +8,21 @@ Shade.Light.diffuse = function(light_opts)
         return v.type.equals(Shade.Types.vec4) ? v.swizzle("xyz").div(v.at(3)) : v;
     }
     var light_diffuse = light_opts.color;
+    if (light_diffuse.type.equals(Shade.Types.vec4))
+        light_diffuse = light_diffuse.swizzle("xyz");
     var light_pos = vec3(light_opts.light_position);
 
     return function(material_opts) {
-        material_opts = _.defaults(material_opts || {}, { 
+        material_opts = _.defaults(material_opts || {}, {
             two_sided: false
         });
         var vertex_pos = vec3(material_opts.vertex);
         var material_color = material_opts.material;
+        if (material_color.type.equals(Shade.Types.vec4))
+            material_color = material_color.swizzle("xyz");
 
-        var vertex_normal = (material_opts.normal.type.equals(Shade.Types.vec4) ? 
-                             material_opts.normal.swizzle("xyz") : 
+        var vertex_normal = (material_opts.normal.type.equals(Shade.Types.vec4) ?
+                             material_opts.normal.swizzle("xyz") :
                              material_opts.normal).normalize();
 
         var N = vertex_normal;
@@ -27,6 +31,9 @@ Shade.Light.diffuse = function(light_opts)
                                        L.dot(N).abs(),
                                        L.dot(N)), 0);
 
-        return Shade.add(v.mul(light_diffuse).mul(material_color));
+        var c = Shade.add(v.mul(light_diffuse).mul(material_color));
+
+        return material_opts.material.type.equals(Shade.Types.vec4) ?
+            Shade.vec(c, material_opts.material.a()) : c;
     };
 };
