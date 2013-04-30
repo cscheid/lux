1bcd17b2dfa46ff7aa28b9b6062a8e5011467d0e
Carlos Scheidegger <cscheid@research.att.com>
2013-04-30 15:56:37 -0400

    more work on lessons and demos

--- demos/tutorial/lesson6/lesson6.js
+++ demos/tutorial/lesson6/lesson6.js
@@ -14,13 +14,29 @@ function batches(opts)
 
     var light_position = light_model_mat(Shade.vec(0, 0, 2));
 
+    var ambient_parameter = Shade.parameter("float", 0.3);
     var ambient_light = Shade.Light.ambient({ 
-        color: Shade.vec(0.3, 0.3, 0.3)
+        color: Shade.vec(1,1,1).mul(ambient_parameter)
     });
 
+    var diffuse_parameter = Shade.parameter("float", 1);
+    var diffuse_light_color = Shade.vec(1,1,1).mul(diffuse_parameter);
     var diffuse_light = Shade.Light.diffuse({
         light_position: light_position,
-        color: Shade.color('white')
+        color: diffuse_light_color
+    });
+
+    Lux.UI.parameter_slider({
+        parameter: ambient_parameter,
+        element: "#ambient",
+        min: 0,
+        max: 1
+    });
+    Lux.UI.parameter_slider({
+        parameter: diffuse_parameter,
+        element: "#diffuse",
+        min: 0,
+        max: 1
     });
 
     // draw a little flying lamp to make it somewhat more obvious where the light is coming from
@@ -28,9 +44,9 @@ function batches(opts)
     var lamp_batch = Lux.bake(sphere, {
         position: camera
                       (Shade.translation(light_position.swizzle("xyz")))
-                      (Shade.scaling(0.1))
+                      (Shade.scaling(0.05))
                       (sphere.vertex),
-        color: Shade.color('white')
+        color: Shade.vec(diffuse_light_color, 1)
     });
 
     var material_opts = {
