d22a2af8852faeb95f3ae5f6681f13510cc568b4
Carlos Scheidegger <cscheid@research.att.com>
2013-06-07 18:07:42 -0400

    conditional actor for new scene system

--- src/lux/conditional_batch.js
+++ src/lux/conditional_batch.js
@@ -6,3 +6,18 @@ Lux.conditional_batch = function(batch, condition)
         }
     };
 };
+
+Lux.conditional_actor = function(opts)
+{
+    var appearance = opts.appearance;
+    var model = opts.model;
+    var condition = opts.condition;
+    var actor = Lux.actor(opts);
+    actor.dress = function(scene) {
+        var xform = scene.get_transform();
+        var this_appearance = xform(appearance);
+        var batch = Lux.bake(model, this_appearance);
+        return Lux.conditional_batch(batch, condition);
+    };
+    return actor;
+};
