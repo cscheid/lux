5604cdd753f48760f90a3b5ad2f44ea1f33529a5
Carlos Scheidegger <cscheid@research.att.com>
2013-06-12 18:07:28 -0400

    tweak zoom amount when dblclicking

--- src/lux/ui/center_zoom_interactor.js
+++ src/lux/ui/center_zoom_interactor.js
@@ -53,7 +53,7 @@ Lux.UI.center_zoom_interactor = function(opts)
 
     function dblclick(event) {
         internal_move(result.width/2-event.offsetX, event.offsetY-result.height/2);
-        zoom.set(zoom.get() * 1.5);
+        zoom.set(zoom.get() * 2);
         internal_move(event.offsetX-result.width/2, result.height/2-event.offsetY);
         Lux.Scene.invalidate();
         opts.dblclick(event);
