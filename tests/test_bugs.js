2e9dad03a7daaec9ab58bae55ac47b9db259b90e
Carlos Scheidegger <cscheid@research.att.com>
2013-07-26 17:26:39 -0400

    still failing a test...

--- tests/test_bugs.js
+++ tests/test_bugs.js
@@ -18,7 +18,7 @@ test("Tests from previous Lux bugs", function() {
     var prog = { position: node_exp,
                  color: Shade.vec(0,0,0,1) };
 
-    ok(Shade.program(prog), "backface culling should not crash optimizer");
+    ok(Shade.program(prog), "discard expressions should be hoisted to fragment program");
 
     var condition2 = {"type":"gt","guid":245,"parents":[
         {"type":"swizzle{z}","guid":243,"parents":[
@@ -27,19 +27,18 @@ test("Tests from previous Lux bugs", function() {
                     {"type":"cross","guid":237,"parents":[
                         {"type":"dFdx","guid":235,"parents":[
                             {"type":"div","guid":234,"parents":[
-                                {"type":"swizzle{xyz}","guid":232,"parents":[{"type":"reference","guid":227}]},
+                                {"type":"swizzle{xyz}","guid":232,"parents":[exp]},
                                 {"type":"swizzle{w}","guid":233,"parents":[{"type":"reference","guid":227}]}]}]},
                         {"type":"dFdy","guid":236,"parents":[{"type":"reference","guid":234}]}]}]},
                 {"type":"vec","guid":241,"parents":[
                     {"type":"constant","guid":239,"parents":[],"values":[0]},
                     {"type":"constant","guid":240,"parents":[],"values":[0]},
-                    {"type":"ifelse","guid":231,"parents":[
-                        {"type":"constant","guid":228,"parents":[],"values":[true]},
-                        {"type":"constant","guid":229,"parents":[],"values":[1]},
-                        {"type":"constant","guid":230,"parents":[],"values":[-1]}]}]}]}]},
+                    {"type":"constant","guid":241,"parents":[],"values":[1]}]}]}]},
         {"type":"constant","guid":244,"parents":[],"values":[0]}]};
-    var prog2 = { position: Shade.Debug.from_json({"type":"discard_if","guid":246,"parents":[exp,condition2]}),
-                  color: Shade.vec(0,0,0,1) };
 
-    ok(Shade.program(prog), "backface culling should not crash optimizer");
+    var prog2 = { position: Shade.vec(0,0,0,0),
+                  color: Shade.Debug.from_json(condition2).ifelse(Shade.vec(1,0,0,0),
+                                                                  Shade.vec(0,1,0,0)) };
+
+    ok(Shade.program(prog2), "backface culling should not crash optimizer");
 });
