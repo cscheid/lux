f51a36019816c32351673e50765f8a8a8f809519
Carlos Scheidegger <cscheid@research.att.com>
2013-07-27 19:29:49 -0400

    fix cull_backface

--- src/shade/threed/cull_backface.js
+++ src/shade/threed/cull_backface.js
@@ -1,7 +1,6 @@
-Shade.ThreeD.cull_backface = Shade(function(position, ccw)
+Shade.ThreeD.cull_backface = Shade(function(position, normal, ccw)
 {
     if (_.isUndefined(ccw)) ccw = Shade(true);
     ccw = ccw.ifelse(1, -1);
-    var n = Shade.ThreeD.normal(position);
-    return position.discard_if(n.cross(Shade.vec(0,0,ccw)).z().gt(0));
+    return position.discard_if(normal.dot(Shade.vec(0,0,ccw)).le(0));
 });
