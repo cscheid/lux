971f6e3868c8be9af73636974f73ecb54a5b20f5
Carlos Scheidegger <cscheid@research.att.com>
2013-08-27 00:38:16 -0400

    update

--- demos/osm/osm.js
+++ demos/osm/osm.js
@@ -2,22 +2,13 @@ $().ready(function () {
     var canvas = document.getElementById("webgl");
     var gl = Lux.init({
         clearColor: [0,0,0,0.1],
-        mousedown: function(event) {
-            var result = globe.mousedown(event);
-            return result;
-        },
-        mousemove: function(event) {
-            var result = globe.mousemove(event);
-            return result;
-        },
-        mouseup: function(event) {
-            var result = globe.mouseup(event);
-            return result;
-        }
+        mousedown: function(event) { return globe.mousedown(event); },
+        mousemove: function(event) { return globe.mousemove(event); },
+        mouseup:   function(event) { return globe.mouseup(event); }
     });
 
     var globe_zoom = Shade.parameter("float", 1.0);
-    var view_proj = Shade.Camera.perspective({
+    var perspective_scene = Lux.Scene.Transform.Camera.perspective({
         look_at: [Shade.vec(0, 0,  6),
                   Shade.vec(0, 0, -1),
                   Shade.vec(0, 1,  0)],
@@ -25,13 +16,9 @@ $().ready(function () {
     });
 
     var globe = Lux.Marks.globe({ 
-        view_proj: view_proj,
         zoom: globe_zoom
-        // ,
-        // tile_pattern: function(zoom, x, y) {
-        //     return "http://oatile1.mqcdn.com/tiles/1.0.0/sat/" + zoom + "/" + x + "/" + y + ".jpg";
-        // }
     });
 
-    Lux.Scene.add(globe);
+    Lux.Scene.add(perspective_scene);
+    perspective_scene.add(globe);
 });
