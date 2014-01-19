8279da40c25df9bc082dee2d32d6e57e12887823
Carlos Scheidegger <cscheid@research.att.com>
2014-01-18 19:38:45 -0500

    remove commented-out code

--- lux.js
+++ lux.js
@@ -16915,15 +16915,6 @@ Lux.Scene.remove = function(obj, ctx)
     }
     var scene = ctx._lux_globals.scene;
     scene.remove(obj);
-
-    // var i = scene.indexOf(obj);
-
-    // if (i === -1) {
-    //     return undefined;
-    // } else {
-    //     return scene.splice(i, 1)[0];
-    // }
-    // Lux.Scene.invalidate(undefined, undefined, ctx);
 };
 Lux.Scene.render = function()
 {
