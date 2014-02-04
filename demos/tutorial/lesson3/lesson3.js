265e17a6c9d72c63b0f27165d659e887fd364f07
Carlos Scheidegger <cscheid@research.att.com>
2014-02-04 12:57:07 -0500

    tutorial now makes no reference to batches, other minor cleanups

--- demos/tutorial/lesson3/lesson3.js
+++ demos/tutorial/lesson3/lesson3.js
@@ -1,8 +1,5 @@
 $().ready(function () {
-    var ctx = Lux.init({
-        clearColor: [0,0,0,0.2]
-    });
-
+    var ctx = Lux.init({ clearColor: [0,0,0,0.2] });
     var square = Lux.model({
         type: "triangles",
         elements: [0, 1, 2, 0, 2, 3],
@@ -14,8 +11,7 @@ $().ready(function () {
         vertex: [[0,1, -1,-1, 1,-1], 2],
         color: [Shade.color('red'),
                 Shade.color('green'),
-                Shade.color('blue')]
-    });
+                Shade.color('blue')]});
 
     var camera = Shade.Camera.perspective();
     // rotate the objects at 50 degrees/second.
@@ -25,19 +21,19 @@ $().ready(function () {
     Lux.Scene.add(Lux.actor({ 
         model: square, 
         appearance: {
-            position: camera(Shade.translation( 1.5, 0, -6))
-            (Shade.rotation(angle, Shade.vec(1, 0, 0)))
-            (square.vertex),
-            color: Shade.color('#88f')
-    }}));
+            position: camera(
+                Shade.translation( 1.5, 0, -6)(
+                    (Shade.rotation(angle, Shade.vec(1, 0, 0))(
+                        square.vertex)))),
+            color: Shade.vec(now.sin().add(1).div(2), 0.5, 0.5, 1)}}));
+
     Lux.Scene.add(Lux.actor({
         model: triangle, 
         appearance: {
-            position: camera(Shade.translation(-1.5, 0, -6))
-            (Shade.rotation(angle, Shade.vec(0, 1, 0)))
-            (triangle.vertex),
-            color: triangle.color
-    }}));
-
+            position: camera(
+                Shade.translation(-1.5, 0, -6)(
+                    (Shade.rotation(angle, Shade.vec(0, 1, 0))(
+                        triangle.vertex)))),
+            color: triangle.color }}));
     Lux.Scene.animate();
 });
