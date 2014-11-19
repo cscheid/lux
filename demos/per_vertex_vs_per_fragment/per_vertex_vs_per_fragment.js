dee95d1cf11eafb8814077d758350b5bd1b97e47
Carlos Scheidegger <cscheid@cs.arizona.edu>
2014-11-19 10:29:54 -0700

    per vertex uses current API

--- demos/per_vertex_vs_per_fragment/per_vertex_vs_per_fragment.js
+++ demos/per_vertex_vs_per_fragment/per_vertex_vs_per_fragment.js
@@ -3,6 +3,7 @@ var gl;
 var sphere, sphereProg, sphereDrawable;
 var currentMode;
 var currentRotation = 0;
+var perVertex;
 var mvp;
 var Models = Lux.Models;
 
@@ -21,66 +22,46 @@ var brewerColormap = S.Utils.lerp([
     S.vec(1/255, 102/255, 94/255)
 ]);
 
-function display()
-{
-    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
-    gl.clearDepth(1.0);
-    gl.clearColor(0,0,0,0.5);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    gl.depthFunc(gl.LESS);
-    gl.enable(gl.DEPTH_TEST);
-
-    // ortho(-5, 5, -5, 5, -5, 5);
-    var pMatrix = Lux.perspective(45, 720/480, 1.0, 100.0);
-    var mvMatrix = mat4.product(Lux.translation(0, 0, -5),
-                                Lux.rotation(currentRotation, [0,1,0]));
-    mvp.set(mat4.product(pMatrix, mvMatrix));
-    sphereDrawable[currentMode].draw();
-}
-
 function initWebgl()
 {
     mvp = S.parameter("mat4");
+    perVertex = S.parameter("bool", true);
 
     sphere = Models.sphere(10, 10);
 
     var sphereModelVertex = sphere.vertex;
     var position = mvp.mul(sphereModelVertex);
-    var vVal = S.perVertex(sphereModelVertex.swizzle("x").add(1).mul(0.5));
+    var val = sphereModelVertex.swizzle("x").add(1).mul(0.5);
+    var vVal = S.perVertex(val);
 
-    var p1 = Lux.bake(sphere, { 
-        position: position,
-        color: S.vec(S.perVertex(brewerColormap(vVal)), 1)
-    });
+    var col = S.ifelse(perVertex,
+                       S.vec(S.perVertex(brewerColormap(vVal)), 1),
+                       S.vec(brewerColormap(val), 1));
 
-    var p2 = Lux.bake(sphere, {
-        position: position,
-        color: S.vec(brewerColormap(S.perVertex(vVal)), 1)
-    });
-
-    sphereDrawable = {
-        perVertex: p1,
-        perFragment: p2
-    };
+    Lux.Scene.add(Lux.actor({
+        model: sphere,
+        appearance: {
+            position: position,
+            color: col }}));
 }
 
 $().ready(function() {
     var canvas = document.getElementById("foo");
     currentMode = "per_vertex";
-    $("#vp_button").click(function() {
-        currentMode = "per_vertex";
-    });
-    $("#fp_button").click(function() {
-        currentMode = "per_fragment";
-    });
+    $("#vp_button").click(function() { perVertex.set(true); });
+    $("#fp_button").click(function() { perVertex.set(false); });
     gl = Lux.init();
     initWebgl();
+    var pMatrix = Lux.perspective(45, 720/480, 1.0, 100.0);
+    var mvMatrix = mat4.product(Lux.translation(0, 0, -5),
+                                Lux.rotation(currentRotation, [0,1,0]));
+    mvp.set(mat4.product(pMatrix, mvMatrix));
     var start = new Date().getTime();
-    var f = function () {
-        window.requestAnimationFrame(f, canvas);
+    Lux.Scene.animate(function() {
         var elapsed = new Date().getTime() - start;
         currentRotation = (elapsed / 20) * (Math.PI / 180);
-        display();
-    };
-    f();
+        var mvMatrix = mat4.product(Lux.translation(0, 0, -5),
+                                    Lux.rotation(currentRotation, [0,1,0]));
+        mvp.set(mat4.product(pMatrix, mvMatrix));
+    });
 });
