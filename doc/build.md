16ebdc9ce4627d51408c2200d67824f5d02321c9
Carlos Scheidegger <cscheid@cs.arizona.edu>
2018-04-23 11:04:05 -0700

    minimal babel+webpack that builds, ok

---
+++ doc/build.md
@@ -0,0 +1,5 @@
+The build environment is essentially copied [from
+here](https://www.robinwieruch.de/minimal-react-webpack-babel-setup/)
+(I don't need react but I want to make sure this junk works.)
+
+
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
